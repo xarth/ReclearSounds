985f146390fc9b7c334e234d1c632988b23a0329
David Hunt <david@djhunt.co.uk>
2025-02-28 12:21:39 +0000

    Adding Reclear SoundPack

---
+++ core.lua
@@ -0,0 +1,14 @@
+local LSM = LibStub("LibSharedMedia-3.0");
+
+LSM:Register("sound", "Reclear: Wolololo", "Interface\\Addons\\ReclearSounds\\Sounds\\Wolololo.ogg")
+LSM:Register("sound", "Reclear: LetsGOOOOOO", "Interface\\Addons\\ReclearSounds\\Sounds\\LetsGOOOOOO.ogg")
+LSM:Register("sound", "Reclear: Retard", "Interface\\Addons\\ReclearSounds\\Sounds\\Retard.ogg")
+LSM:Register("sound", "Reclear: Defensive Mode", "Interface\\Addons\\ReclearSounds\\Sounds\\Defensivemode.ogg")
+LSM:Register("sound", "Reclear: Scream", "Interface\\Addons\\ReclearSounds\\Sounds\\Scream.ogg")
+LSM:Register("sound", "Reclear: Classic Boss", "Interface\\Addons\\ReclearSounds\\Sounds\\Classicboss.ogg")
+LSM:Register("sound", "Reclear: Do Mechanics", "Interface\\Addons\\ReclearSounds\\Sounds\\DoMechanics.ogg")
+LSM:Register("sound", "Reclear: Really Easy", "Interface\\Addons\\ReclearSounds\\Sounds\\ReallyEasy.ogg")
+LSM:Register("sound", "Reclear: Boss Not Bad", "Interface\\Addons\\ReclearSounds\\Sounds\\Bossnotbad.ogg")
+LSM:Register("sound", "Reclear: Hate You All", "Interface\\Addons\\ReclearSounds\\Sounds\\hateyouall.ogg")
+LSM:Register("sound", "Reclear: Do DPS", "Interface\\Addons\\ReclearSounds\\Sounds\\dodps.ogg")
+LSM:Register("sound", "Reclear: What Is Happening", "Interface\\Addons\\ReclearSounds\\Sounds\\whatishappening.ogg")
